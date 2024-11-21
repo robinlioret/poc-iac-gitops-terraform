01d5b42491eb5aac8dea1cc98c02e480f2a806d0
Robin LIORET <robin.lioret.epro@gmail.com>
2024-11-21 15:25:42 +0100

    feat: add aws credentials variables

--- terraform/sample-s3-bucket/terraform.tf
+++ terraform/sample-s3-bucket/terraform.tf
@@ -9,6 +9,8 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 
   skip_metadata_api_check = true
   skip_credentials_validation = true
--- terraform/sample-s3-bucket/variables.tf
+++ terraform/sample-s3-bucket/variables.tf
@@ -3,6 +3,16 @@ variable "aws_region" {
   type = string
 }
 
+variable "aws_access_key_id" {
+  type = string
+  sensitive = true
+}
+
+variable "aws_secret_access_key" {
+  type = string
+  sensitive = true
+}
+
 variable "bucket_name" {
   default = "test-bucket"
   type = string
